bc4cb348ba639d51484cc27f05be4cdcc802bc78
Mark Severson <markse@instructure.com>
2014-07-30 10:43:06 -0600

    reporter implemented against firework_client
    supports testem v0.3.22 - v0.6.17

---
+++ index.js
@@ -0,0 +1,40 @@
+var spawn = require('child_process').spawn
+
+function FireworkReporter(opts){
+  var clientOpts = [], DB, URL
+
+  if (DB = process.env.FIREWORK_REPORTER_DB)
+    clientOpts = clientOpts.concat(['-db', DB])
+  if (URL = process.env.FIREWORK_REPORTER_URL)
+    clientOpts = clientOpts.concat(['-url', URL])
+
+  if (clientOpts.length > 0) {
+    this.client = spawn('firework_client', ['-stdin'].concat(clientOpts), {stdio: ['pipe', process.stdout, process.stderr]})
+  }
+}
+FireworkReporter.prototype = {
+  report: function(prefix, data) {
+    if (this.client) {
+      var result = {
+        test: data['name'],
+        success: data['passed'],
+        environment: prefix.replace(/\s+/, '_')
+      }
+      if (data['error']) {
+        if (data['error']['message'])
+          result.details = '' + data['error']['message']
+        if (data['error']['actual'])
+          result.actual = '' + data['error']['actual']
+        if (data['error']['expected'])
+          result.expected = '' + data['error']['expected']
+      }
+
+      this.client.stdin.write(JSON.stringify(result) + '\n')
+    }
+  }
+}
+
+var test_reporters = require('testem/lib/ci/test_reporters')
+test_reporters['firework'] = FireworkReporter
+
+module.exports = FireworkReporter
